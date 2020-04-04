f9ecd90bb7aac4f57b5ceec1bf1371835cce9865
Marcelo Hori <marcelohori@gmail.com>
2020-04-04 01:46:03 -0300

    Adicionar arquivos de projeto.

---
+++ VisualApp/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace VisualApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnMostraResultado = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(12, 12);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(936, 68);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "Carregar Imagem";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 86);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(936, 331);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnMostraResultado
+            // 
+            this.btnMostraResultado.Location = new System.Drawing.Point(12, 423);
+            this.btnMostraResultado.Name = "btnMostraResultado";
+            this.btnMostraResultado.Size = new System.Drawing.Size(936, 50);
+            this.btnMostraResultado.TabIndex = 2;
+            this.btnMostraResultado.Text = "Descrever Imagem";
+            this.btnMostraResultado.UseVisualStyleBackColor = true;
+            this.btnMostraResultado.Click += new System.EventHandler(this.btnMostraResultado_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(3, 479);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(945, 226);
+            this.txtResult.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(948, 814);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnMostraResultado);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnBrowse);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnMostraResultado;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ VisualApp/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.IO;
+
+
+namespace VisualApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        const string subscriptionKey = "122f8037b2684280a871bf9e6e476eba";
+        const string uriBase = "https://visionfiap.cognitiveservices.azure.com/";
+        string imageFilePath = "";
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            
+            OpenFileDialog ofd = new OpenFileDialog();
+            // Filtro para aceitar somente imagens nos formatos JPEG and PNG  
+            ofd.Filter = "JPEG *.jpg|*.jpg|PNG *.png|*.png";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                // Armazena arquivo selecionado em uma variavel generica   
+                imageFilePath = ofd.FileName;
+                // Preview da imagem selecionada no controle pictureBox  
+                pictureBox1.Image = Image.FromFile(imageFilePath);
+            }
+        }
+
+        
+
+        // Metodo que faz a requisicao restful   
+        async Task<string> MakeAnalaysisRequest(string imageFilePath)
+        {
+            // Instancia um novo objeto do tipo HttpClient para realizar o post no Azure  
+            HttpClient client = new HttpClient();
+
+            // Parametros de requisicao 
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+            // Especifica as categorias pesquisadas   
+            string requestParamters = "visualFeatures=Categories,Description,Color&language=pt";
+
+            // Endpoint com os parametros   
+            string uri = uriBase + "vision/v2.1/analyze" + "?" + requestParamters;
+
+            // Cria um objeto para o retorno do Request    
+            HttpResponseMessage responseMessage;
+
+            // Tamanho da imagem selecionada   
+            byte[] dataByte = GetImageAsBytesArray(imageFilePath);
+
+            
+            using (ByteArrayContent content = new ByteArrayContent(dataByte))
+            {
+                // Define o conteudo 
+                content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/octet-stream");
+
+                // Envia o conteudo do Request
+                responseMessage = await client.PostAsync(uri, content);
+
+                // Recebe a resposta do request enviado   
+                string contentString = await responseMessage.Content.ReadAsStringAsync();
+
+                // Retorna resultado
+                return contentString;
+            }
+
+        }
+
+        // Metodo que verifica tamanho da imagem
+        private byte[] GetImageAsBytesArray(string imageFilePath)
+        {
+            // Cria um file stream para acessar os bytes do arquivo selecionado   
+            FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
+            // Cria um leitor binario   
+            BinaryReader reader = new BinaryReader(fileStream);
+            // Retorna os bytes do arquivo   
+            return reader.ReadBytes((int)fileStream.Length);
+        }
+
+        private async void btnMostraResultado_Click(object sender, EventArgs e)
+        {
+            // Pega o resultado 
+            string result = await MakeAnalaysisRequest(imageFilePath);
+
+            // Mostra resultado no textbox   
+            txtResult.Text = result;
+        }
+    }
+}
